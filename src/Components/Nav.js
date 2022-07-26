63cb186a542521f37761bd2b6fb2116d84d26a48
Tanmay2510 <comparisionthing@gmail.com>
2022-07-26 23:58:44 +0530

    Added ess for nav page

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import AnotherNav from "./AnotherNav.js"
+import "./Nav.css";
+import Bluebub  from "../icon/Bluebub";
+import  Orangebub from "../icon/Orangebub";
+
+import Redbub from "../icon/Redbub";
+import Greenbub from "../icon/Greenbub";
+
 function Nav() {
+
   return (
     <div>
     <AnotherNav />
+    
+    <div className='bggg' >
+     {<Bluebub />}
+     {<Redbub />}
+
+     {<Orangebub />}
+
+     
+    </div>
+    {<Greenbub />}
     
     </div>
   )
