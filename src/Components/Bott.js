f48750eb1e81dd23ace0af30f82be7cddfe1033a
Tanmay2510 <comparisionthing@gmail.com>
2022-08-22 15:56:23 +0530

    RESP v3

--- src/Components/Bott.js
+++ src/Components/Bott.js
@@ -6,7 +6,7 @@ function Bott() {
   return (
     <div className='last'>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="black" fill-opacity="1" d="M0,96L60,101.3C120,107,240,117,360,112C480,107,600,85,720,85.3C840,85,960,107,1080,112C1200,117,1320,107,1380,101.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-    <div style={{marginTop:"-100px"}}>
+    <div style={{marginTop:"-7.5vw"}}>
     <Lowernav />
     </div>
     
